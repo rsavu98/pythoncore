6eb62c2ac20da2388a88887ff1837ca682c90d27
rsavu98 <savuliak9852@gmail.com>
2020-02-08 19:09:11 +0200

    Added HW_1_1

---
+++ HW_1_1.py
@@ -0,0 +1,7 @@
+first_number = int (input ('Type first number: ')) 
+second_number = int (input ('Type second number: '))
+
+print(f'{first_number} + {second_number} = {first_number + second_number}')
+print(f'{first_number} - {second_number} = {first_number - second_number}')
+print(f'{first_number} * {second_number} = {first_number * second_number}')
+print(f'{first_number} / {second_number} = {first_number / second_number}')
