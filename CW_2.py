3788418669b046f7f7392c0a62955ee2f46a3c3f
Ruslana Savuliak <savuliak9852@gmail.com>
2020-02-11 15:24:36 +0200

    Changes to CW_2

--- CW_2.py
+++ CW_2.py
@@ -1,29 +1,48 @@
 #Task 1.1
-a = input('first number: ')
-b = input('second number: ')
+a = int(input('first number: '))
+b = int(input('second number: '))
 if a > b:
   print(f'{a} is bigger than {b}')
 elif a < b:
   print(f'{b} is bigger than {a}')
 
-  #Task 1.2
+#Task 1.2
 a = int(input('type any number: '))
 if a % 2 == 0:
   print(f'{a} is even number')
 else:
   print(f'{a} is odd number')
 
-  #Task 1.3
-  a = int(input('type any number: '))
+#Task 1.3
+a = int(input('type any number: '))
 result = 1
 for x in range(1, a+1):
   result *= x
- 
-print(f'factorial of {a} is:',result)
+print(f'factorial of {a} is: {result}')
 
-#Task 2.1
+#Task 2.1a
 i = 1
 while i <100:
   if i %2 == 0:
     print(i)
   i += 1
+
+#Task 2.1b
+i = 1
+for i in range(1,100):
+  if i %2 == 0:
+    print(i)
+  i += 1
+
+#Task 2.2.a
+for i in range(100):
+  if i %2 == 0:
+    continue
+  print(i)
+
+#Task 2.2.b
+i=0
+while i < 100:
+  if i %2 ==1:
+    print(i)
+  i += 1    
