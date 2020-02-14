0d35073de2cba5598186f8adee10337babd11edf
Ruslana Savuliak <savuliak9852@gmail.com>
2020-02-14 16:02:45 +0200

    Added CW_3

---
+++ CW_3.py
@@ -0,0 +1,54 @@
+#Task1
+# n = int(input('How many integers in your list? '))
+# mylist = []
+# for i in range(n):
+#     mylist.append(int(input('Enter the new number ')))
+# print(max(mylist))
+# print(min(mylist))
+
+#Task2
+# mylist1 = []
+# mylist2 = []
+# mylist3 = []
+# for i in range(1,11):
+#     if i %2 == 0:
+#         mylist1.append(i)
+#     elif i %3 == 0:
+#         mylist2.append(i)
+#     else:
+#         mylist3.append(i)
+# print(mylist1)
+# print(mylist2)
+# print(mylist3)
+
+#Task3
+# number = int(input('Enter your number: '))
+# factorial = 1
+# for x in range (1, number+1):
+#     factorial *= x 
+# print(factorial)
+
+#Task4
+# login = input('Enter your login: ')
+# true = 'First'
+# while login != true:
+#     print('Try again')
+#     login = input('Enter your login: ')
+# else:
+#     print('Welcome!')
+
+#Task5
+# number = int(input('Enter number: '))
+# while number >= 1:
+#     print('Number is positive')
+#     number = int(input('Enter number: '))
+# print('The end')
+
+#Task6
+# number = list(range(int(input('How many numbers? '))))
+# for i in number:
+#     x = int(input('Enter number:'))
+#     if x <= 0:
+#       print('The end')
+#       break
+# print(number)
